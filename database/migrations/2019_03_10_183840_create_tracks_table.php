8f46b1f692681b324f8dffa45a60fa9cd0f2636d
Basemera <phiona.basemera@andela.com>
2019-03-10 22:34:15 +0300

    Add migrations

---
+++ database/migrations/2019_03_10_183840_create_tracks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('album_id');
+            $table->integer('genre_id');
+            $table->string('title');
+            $table->integer('ratings');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
